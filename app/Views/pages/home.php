96caf8f05218fc30b2ad72d8a30935065ff34729
gunawanzakiah <gunawanzakiah@gmail.com>
2020-06-15 22:34:58 +0700

    membuat fitur layout

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -1,11 +1,13 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
 <div class="container">
   <div class="row">
     <div class="col">
 
       <h1>Hello, world!</h1>
-      <?php
-      d($test);
-      ?>
+
     </div>
   </div>
 </div>
+<?= $this->endSection(); ?>
