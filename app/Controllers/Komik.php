41209de742a439c0f74d18b29ced56c9d65ba26c
gunawanzakiah <gunawanzakiah@gmail.com>
2020-06-17 22:05:06 +0700

    create KomikModel.php in app/Models

--- app/Controllers/Komik.php
+++ app/Controllers/Komik.php
@@ -1,14 +1,28 @@
-<?php 
+<?php
 
 namespace App\Controllers;
 
+use App\Models\KomikModel;
+
 class Komik extends BaseController
 {
-  public function index(){
+  protected $komikModel;
+  public function __construct()
+  {
+    $this->komikModel = new KomikModel();
+  }
+  public function index()
+  {
+    $komik = $this->komikModel->findAll();
 
     $data = [
-      'title' => 'Daftar Komik'
+      'title' => 'Daftar Komik',
+      'komik' =>  $komik
     ];
-    return view ('komik/index', $data);
+
+    //$komikModel = new \App\Models\KomikModel();
+    //$komikModel = new KomikModel();
+
+    return view('komik/index', $data);
   }
 }
--- app/Views/komik/index.php
+++ app/Views/komik/index.php
@@ -15,15 +15,17 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td><img src="/img/naruto.jpg" alt="" class="sampul"></td>
-            <td>Naruto</td>
-            <td>
-              <a href="" class="btn btn-success">Detail</a>
-            </td>
-          </tr>
-
+          <?= $i = 1; ?>
+          <?php foreach ($komik as $k) : ?>
+            <tr>
+              <th scope="row"><?= $i++; ?></th>
+              <td><img src="/img/<?= $k['sampul']; ?>" alt="" class="sampul"></td>
+              <td><?= $k['judul']; ?></td>
+              <td>
+                <a href="" class="btn btn-success">Detail</a>
+              </td>
+            </tr>
+          <?php endforeach; ?>
         </tbody>
       </table>
 
